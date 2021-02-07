781cac10dc895376c2f9f85be24395a6090d2c5c
mehedi-hasan-anik <anikm2283@gmail.com>
2021-02-07 18:28:00 +0600

    addAlert

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
 document.getElementById('button').addEventListener("click", function () {
     const foodName = document.getElementById('inputFoodName').value;
+
+    if(foodName ===''){
+        alert("enter food name");
+    }
     
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    
+    else{
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(res => res.json())
         .then(data => display(data))
 
@@ -11,8 +17,8 @@ document.getElementById('button').addEventListener("click", function () {
         const meal = data.meals[0];
         meal.forEach(element => {
             parent.innerHTML = `
-            <h1>${element.strMeal} </h1>
             <img src="${element.strMealThumb}" style="width:200px;">
+            <h1>${element.strMeal} </h1>
 
             `
             parent.onclick= about;
@@ -40,6 +46,7 @@ document.getElementById('button').addEventListener("click", function () {
 
 
     }
+    }
 
 })
 
@@ -47,7 +54,12 @@ document.getElementById('button').addEventListener("click", function () {
 
 document.getElementById('button').addEventListener("click", function () {
     const foodName = document.getElementById('inputFoodName').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    if(foodName === '')
+    {
+        alert("enter food name");
+    }
+    else{
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(Response => Response.json())
         .then(data => displaySameFood(data))
 
@@ -58,8 +70,8 @@ document.getElementById('button').addEventListener("click", function () {
         meal.forEach(element => {
             const divName = document.createElement('div');
             divName.innerHTML = `
-            <h1>${element.strMeal} </h1>
             <img src="${element.strMealThumb}" style="width:200px;">
+            <h1>${element.strMeal} </h1>
 
             `
             divName.onclick = displayDetail;
@@ -67,9 +79,8 @@ document.getElementById('button').addEventListener("click", function () {
                 divName.innerHTML = `
               <img src="${element.strMealThumb}" style="width:200px;">
               <h1>${element.strMeal} </h1>
-
              <ul class="list-unstyled">
-             
+
              <li> <span>${element.strMeasure1}</span> ${element.strIngredient1} </li>
              <li> <span>${element.strMeasure2}</span> ${element.strIngredient2} </li>
              <li> <span>${element.strMeasure3}</span> ${element.strIngredient3} </li>
@@ -88,6 +99,7 @@ document.getElementById('button').addEventListener("click", function () {
 
         })
 
+    }
     }
 })
 
