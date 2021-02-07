5fd0fc27904aba4560231f4e987b7fbf8ac7679b
mehedi-hasan-anik <anikm2283@gmail.com>
2021-02-07 13:23:05 +0600

    second

--- fastLater.js
+++ fastLater.js
@@ -10,13 +10,32 @@ document.getElementById('button').addEventListener("click", function () {
         const meal = food.meals;
         meal.forEach(element => {
             const li = document.createElement('li');
-
             li.innerHTML = `
             <h1>${element.strMeal} </h1>
             <img src="${element.strMealThumb}" style="width:200px;">
 
 
             `
+            li.onclick = displayDetail;
+            function displayDetail(){
+              li.innerHTML = `
+              <h1>${element.strMeal} </h1>
+              <img src="${element.strMealThumb}" style="width:200px;">
+
+              <h3> <span>${element.strMeasure1}</span> ${element.strIngredient1} </h3>
+              <h3> <span>${element.strMeasure2}</span> ${element.strIngredient2} </h3>
+              <h3> <span>${element.strMeasure3}</span> ${element.strIngredient3} </h3>
+              <h3> <span>${element.strMeasure4}</span> ${element.strIngredient4} </h3>
+              <h3> <span>${element.strMeasure5}</span> ${element.strIngredient5} </h3>
+              <h3> <span>${element.strMeasure6}</span> ${element.strIngredient6} </h3>
+              <h3> <span>${element.strMeasure7}</span> ${element.strIngredient7} </h3>
+              <h3> <span>${element.strMeasure8}</span> ${element.strIngredient8} </h3>
+
+
+              `
+            }
+               
+            
             parent2.appendChild(li);
 
 
