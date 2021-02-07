02644e77250a97f8f2ead9af026e6e4414bd6733
mehedi-hasan-anik <anikm2283@gmail.com>
2021-02-07 17:46:29 +0600

    bootstrap

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 
 document.getElementById('button').addEventListener("click", function () {
     const foodName = document.getElementById('inputFoodName').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
-    .then(Response => Response.json())
-    .then(data => console.log(data))
-
+    
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(res => res.json())
         .then(data => display(data))
 
     const display = data => {
         const parent = document.getElementById('namePhoto');
-        const meal = data.meals;
+        const meal = data.meals[0];
         meal.forEach(element => {
             parent.innerHTML = `
             <h1>${element.strMeal} </h1>
@@ -21,16 +18,20 @@ document.getElementById('button').addEventListener("click", function () {
             parent.onclick= about;
             function about(){
                 parent.innerHTML = `
-                <h1>${element.strMeal} </h1>
                 <img src="${element.strMealThumb}" style="width:200px;">
-                <h3> <span>${element.strMeasure1}</span> ${element.strIngredient1} </h3>
-                <h3> <span>${element.strMeasure2}</span> ${element.strIngredient2} </h3>
-                <h3> <span>${element.strMeasure3}</span> ${element.strIngredient3} </h3>
-                <h3> <span>${element.strMeasure4}</span> ${element.strIngredient4} </h3>
-                <h3> <span>${element.strMeasure5}</span> ${element.strIngredient5} </h3>
-                <h3> <span>${element.strMeasure6}</span> ${element.strIngredient6} </h3>
-                <h3> <span>${element.strMeasure7}</span> ${element.strIngredient7} </h3>
-                <h3> <span>${element.strMeasure8}</span> ${element.strIngredient8} </h3>
+                <h1>${element.strMeal} </h1>
+                <ul class="list-unstyled">
+
+                <li> <span>${element.strMeasure1}</span> ${element.strIngredient1} </li>
+                <li> <span>${element.strMeasure2}</span> ${element.strIngredient2} </li>
+                <li> <span>${element.strMeasure3}</span> ${element.strIngredient3} </li>
+                <li> <span>${element.strMeasure4}</span> ${element.strIngredient4} </li>
+                <li> <span>${element.strMeasure5}</span> ${element.strIngredient5} </li>
+                <li> <span>${element.strMeasure6}</span> ${element.strIngredient6} </li>
+                <li> <span>${element.strMeasure7}</span> ${element.strIngredient7} </li>
+                <li> <span>${element.strMeasure8}</span> ${element.strIngredient8} </li>
+                
+                </ul>
     
                 `
             }
@@ -46,7 +47,7 @@ document.getElementById('button').addEventListener("click", function () {
 
 document.getElementById('button').addEventListener("click", function () {
     const foodName = document.getElementById('inputFoodName').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(Response => Response.json())
         .then(data => displaySameFood(data))
 
@@ -64,17 +65,21 @@ document.getElementById('button').addEventListener("click", function () {
             divName.onclick = displayDetail;
             function displayDetail(){
                 divName.innerHTML = `
-              <h1>${element.strMeal} </h1>
               <img src="${element.strMealThumb}" style="width:200px;">
+              <h1>${element.strMeal} </h1>
 
-              <h3> <span>${element.strMeasure1}</span> ${element.strIngredient1} </h3>
-              <h3> <span>${element.strMeasure2}</span> ${element.strIngredient2} </h3>
-              <h3> <span>${element.strMeasure3}</span> ${element.strIngredient3} </h3>
-              <h3> <span>${element.strMeasure4}</span> ${element.strIngredient4} </h3>
-              <h3> <span>${element.strMeasure5}</span> ${element.strIngredient5} </h3>
-              <h3> <span>${element.strMeasure6}</span> ${element.strIngredient6} </h3>
-              <h3> <span>${element.strMeasure7}</span> ${element.strIngredient7} </h3>
-              <h3> <span>${element.strMeasure8}</span> ${element.strIngredient8} </h3>
+             <ul class="list-unstyled">
+             
+             <li> <span>${element.strMeasure1}</span> ${element.strIngredient1} </li>
+             <li> <span>${element.strMeasure2}</span> ${element.strIngredient2} </li>
+             <li> <span>${element.strMeasure3}</span> ${element.strIngredient3} </li>
+             <li> <span>${element.strMeasure4}</span> ${element.strIngredient4} </li>
+             <li> <span>${element.strMeasure5}</span> ${element.strIngredient5} </li>
+             <li> <span>${element.strMeasure6}</span> ${element.strIngredient6} </li>
+             <li> <span>${element.strMeasure7}</span> ${element.strIngredient7} </li>
+             <li> <span>${element.strMeasure8}</span> ${element.strIngredient8} </li>
+             
+             </ul>
 
               `
             }
